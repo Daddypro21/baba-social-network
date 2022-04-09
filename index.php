18877d3d2432993cd96c02f760311515d24264be
Daddypro21 <sndemapro@gmail.com>
2022-04-09 10:28:53 +0100

    creation de la bar de navigation

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- ICONSCOUT CDN -->
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
+    <!-- ICONSCOUT CDN -->
+    <link rel="stylesheet" href="style.css">
+    <title>baba-socialnetwork - using html,css,js and php</title>
+</head>
+<body>
+    
+   
+    <!--
+    @author Slime Ndema M "Daddypro21" < sndemapro@gmail.com>
+     WebSite :  wibegin.com
+     github : https://github.com/Daddypro21/baba-social-network
+     to create this web site or this web page i have used :
+     html5,css3,js, and php ,there is any code php but you
+     can see my file is a php file
+
+    -->
+        <nav>
+            <div class="container">
+                <h2 class="log">
+                    baba social network
+                </h2>
+
+                <div class="search-bar">
+                    <i class="uil uil-search"></i>
+                    <input type="search" placeholder="Search your baba friends">
+
+                    <div class="create">
+                        <label for="create-post">Create</label>
+                        <div class="profile-picture">
+                            <img src="./images/profile-1.jpg" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </nav>
+
+</body>
+</html>
